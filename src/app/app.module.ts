a72b5f4335b34e631e1f91242666ed42a2ed411d
ryanzam <ryanzam2005@yahoo.com>
2017-10-06 10:57:03 +0300

    items view part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductPriceComponent } from './product-price/product-price.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemListComponent
+    ItemListComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    ProductPriceComponent,
+    ProductDepartmentComponent
   ],
   imports: [
     BrowserModule,
